01f72123bb76ba87786900c74196b78efc9be9cf
David Johnston <davidajohnston2000@gmail.com>
2015-11-06 23:52:06 +0000

    Set up project to allow unit tests to run Spring controllers.

--- src/test/java/com/taselectfc/controllers/FixtureControllerTest.java
+++ src/test/java/com/taselectfc/controllers/FixtureControllerTest.java
@@ -2,11 +2,12 @@ package com.taselectfc.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,24 +16,30 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.taselectfc.dao.FixtureDAO;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:TASelectFC-servlet.xml" })
 @WebAppConfiguration
-public class HomeControllerTest {
+public class FixtureControllerTest {
 
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Mock
+    private FixtureDAO fixtureDAO;
+
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
-    public void shouldGetHomePageAtRoot() throws Exception {
-        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("TASelectFC"));
+    public void shouldGetAllFixturesFromTheDAO() throws Exception {
+        mockMvc.perform(get("/fixtures")).andExpect(status().isOk());
     }
 
 }
